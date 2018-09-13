bd604a2388e9c88e98c4fb779f0180173be5ba43
Cory Perkins <jcperkins12@gmail.com>
2018-09-12 20:51:40 -0500

    Added Cy1_RestrictedTypes code and compiled files

--- run_gravity.py
+++ run_gravity.py
@@ -1,5 +1,6 @@
 from PurePython import gravity
 from Cy0_CompiledPython import gravity_cy0
+from Cy1_RestrictTypes import gravity_cy1
 
 
 @profile
@@ -9,12 +10,20 @@ def run_python(time_span, n_steps):
     return [(p.x, p.y, p.z), (p.vx, p.vy, p.vz)]
 
 
-print(run_python(100, 1000))
+print(run_python(100000, 10000000))
 
 @profile
 def run_cython_0(time_span, n_steps):
     p = gravity_cy0.Planet()
-    gravity_cy0.step_time(gravity.Planet(), time_span, n_steps)
+    gravity_cy0.step_time(gravity_cy0.Planet(), time_span, n_steps)
     return [(p.x, p.y, p.z), (p.vx, p.vy, p.vz)]
 
-print(run_cython_0(100, 1000))
+print(run_cython_0(100000, 10000000))
+
+@profile
+def run_cython_1(time_span, n_steps):
+    p = gravity_cy1.Planet()
+    gravity_cy1.step_time(gravity_cy1.Planet(), time_span, n_steps)
+    return [(p.x, p.y, p.z), (p.vx, p.vy, p.vz)]
+
+print(run_cython_1(100000, 10000000))
